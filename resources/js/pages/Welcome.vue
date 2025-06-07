337fef1ac081ec9f68a0f0dc8b880168413fd897
Emmanuel_Lopez97 <jesus.ortiz@carsol.mx>
2025-06-06 18:42:57 -0600

    Se acomodo el spline viewer para que se vea mejor la imagen de inicio

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -34,24 +34,32 @@ onMounted(() => {
             <spline-viewer url="https://prod.spline.design/L9S3PGoLfEwLpSmB/scene.splinecode"
                 class="absolute inset-0 z-0"></spline-viewer>
 
-            <div class="relative z-10 flex min-h-screen flex-col items-center justify-center selection:text-white">
+            <div class="relative flex min-h-screen flex-col items-center justify-center selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end space-x-4">
-                            <Link v-if="$page.props.auth.user" :href="route('adminproyectos')"
-                                class="text-xl rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                            Regresar a la administración de DevFleet
+                        <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end space-x-4 z-10 relative">
+                            <Link
+                                v-if="$page.props.auth.user"
+                                :href="route('adminproyectos')"
+                                class="text-xl rounded-md px-3 py-2 bg-gray-200 text-gray-800 ring-1 ring-transparent transition hover:bg-gray-300 hover:text-black focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Volver a DevFleet
                             </Link>
 
                             <template v-else>
-                                <Link :href="route('login')"
-                                    class="text-xl rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                Iniciar sesión
+                                <Link
+                                    :href="route('login')"
+                                    class="text-xl rounded-md px-3 py-2 bg-gray-200 text-gray-800 ring-1 ring-transparent transition hover:bg-gray-300 hover:text-black focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Iniciar sesión
                                 </Link>
 
-                                <Link v-if="canRegister" :href="route('register')"
-                                    class="text-xl rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                Registrarse
+                                <Link
+                                    v-if="canRegister"
+                                    :href="route('register')"
+                                    class="text-xl rounded-md px-3 py-2 bg-gray-200 text-gray-800 ring-1 ring-transparent transition hover:bg-gray-300 hover:text-black focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Registrarse
                                 </Link>
                             </template>
                         </nav>
